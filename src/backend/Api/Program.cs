5870ec8f314d9fd322c68d02cd2cc0234c330db1
Sam Hignett <9386922+SamHignett@users.noreply.github.com>
2025-10-21 18:37:37 +0100

    Refactored Setting/Secret Storage & Use for Azure hosting

--- src/backend/Api/Program.cs
+++ src/backend/Api/Program.cs
@@ -1,10 +1,29 @@
+using System.Diagnostics;
 using Api.Registry;
 using Application.Commands.Team;
 using Application.Interfaces;
+using Azure.Identity;
 using Infrastructure.Clients.Team.Tank01;
 using Infrastructure.Registry;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var azureKeyVaultUri = builder.Configuration["KeyVault:VaultUri"];
+if (!string.IsNullOrWhiteSpace(azureKeyVaultUri))
+{
+    try
+    {
+        builder.Configuration.AddAzureKeyVault(new Uri(azureKeyVaultUri), new DefaultAzureCredential());
+    }
+    catch (Exception e)
+    {
+        if (!builder.Environment.IsDevelopment())
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}
 
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(GetTeamInfo.Handler).Assembly));
 
@@ -19,46 +38,41 @@ builder.Services.AddControllers();
 builder.Services.AddHttpClient<ITeamClient, Tank01TeamClient>(client =>
 {
     client.BaseAddress = new Uri("https://tank01-nfl-live-in-game-real-time-statistics-nfl.p.rapidapi.com");
-    client.DefaultRequestHeaders.Add("x-rapidapi-key", builder.Configuration["Tank01:ApplicationKey"]);
+
+    var appKey = builder.Configuration["Tank01:ApplicationKey"];
+    
+    if (!string.IsNullOrEmpty(appKey))
+        client.DefaultRequestHeaders.Add("x-rapidapi-key", appKey);
 });
 
+builder.Services.AddCors(options =>
+{
+    var allowedOrigins = builder.Configuration
+        .GetSection("CORSSettings:AllowedOrigins")
+        .Get<string[]>() ?? [];
+
+    options.AddPolicy("Default", policy =>
+        policy.WithOrigins(allowedOrigins)
+            .AllowAnyHeader()
+            .AllowAnyMethod());
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
-app.UseCors(options =>
-{
-    var origins = builder.Configuration.GetSection("CORSSettings:AllowedOrigins").Get<string[]>();
-
-    if (origins != null)
-        options.WithOrigins(origins)
-            .AllowAnyHeader()
-            .AllowAnyMethod();
-});
 
 app.UseRouting();
+app.UseCors("Default");
+
+
 app.MapControllers();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 
 app.Run();
