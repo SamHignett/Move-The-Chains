0118f006c64a494b626f2c0ef8c1b3c642e857aa
Sam Hignett <9386922+SamHignett@users.noreply.github.com>
2025-10-27 22:04:47 +0000

    added PlayerApi to Portal (#120)

---
+++ src/frontend/src/api/players/PlayerApi.tsx
@@ -0,0 +1,19 @@
+﻿import { useQuery, UseQueryResult } from '@tanstack/react-query';
+import { PlayerInfo } from '@/api/players/responses/PlayerInfo';
+import { useParams } from 'next/navigation';
+
+export function usePlayerInfo(): UseQueryResult<PlayerInfo> {
+  const { playerName } = useParams<{ playerName: string }>();
+
+  if (!playerName)
+    throw new Error('Player name is required to fetch player information.');
+
+  return useQuery({
+    queryKey: ['playerInfo', playerName],
+    queryFn: () =>
+      fetch(`api/player/${playerName}/info`).then((response) =>
+        response.json(),
+      ),
+    staleTime: 1000 * 60 * 5,
+  });
+}
